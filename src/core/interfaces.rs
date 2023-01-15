e3749e3f411990f080ec7d67f25c025838e24be1
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2023-01-15 14:20:04 -0500

    feat: add two new methods (before...,after...)

--- src/core/interfaces.rs
+++ src/core/interfaces.rs
@@ -1,6 +1,6 @@
 use std::io::{self, Stdout};
 
-use super::utils::Action;
+use super::utils::{Action, RenderWidget};
 
 ///Define a Widget to render in each turn of the loop
 pub trait Widget {
@@ -16,5 +16,28 @@ pub trait WidgetRoot {
 }
 
 pub trait WidgetChild<T: Clone>: Widget {
-    fn do_any(&mut self, global_state: T) -> Action;
+    /// Method called by Widget parent, before rendering.
+    ///
+    /// Returns a `RenderWidget`, that indicate if it will render
+    /// ## Arguments:
+    ///
+    /// * `global_state` - Some struct or any type that hold a global state
+    ///
+    /// If the returned value is `RenderWidget::Yes`, then the widget is rendered
+    ///
+    /// Otherwise,not is rendered, and exit the render of the all widgets
+    fn before_render(&mut self, global_state: T) -> RenderWidget;
+
+    /// Method called by Widget parent, after rendering.
+    ///
+    /// Returns a `Action`, that indicate if `continue` with the next widget,
+    ///
+    /// `keep` render with the current widget, and `exit` the render by widget parent.
+    ///
+    /// ## Arguments:
+    ///
+    /// * `global_state` - Some struct or any type that hold a global state
+    ///
+    ///
+    fn after_render(&mut self, global_state: T) -> Action;
 }
--- src/core/utils.rs
+++ src/core/utils.rs
@@ -3,10 +3,17 @@
 ///  - `Exit`: Finish the render
 ///  - `KeepSection`: keep in the same section
 ///
+#[derive(PartialEq, Clone, Debug)]
 pub enum Action {
     Next,
     Exit,
     KeepSection,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum RenderWidget {
+    Yes,
+    No,
 }
 
 pub struct IconAndLabel<'a>(pub &'a str, pub &'a str);
